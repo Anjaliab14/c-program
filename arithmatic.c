05a4f0ffbd748e56cc4513c2775a898899f49b77
Anjaliab14 <82696958+Anjaliab14@users.noreply.github.com>
2021-04-26 13:56:42 +0530

    Add files via upload

---
+++ arithmatic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	float a,b,s,d,m,q;
+	printf("enter the two numbers: ");
+	scanf("%f%f",&a,&b);
+	s=a+b;
+	d=a-b;
+	m=a*b;
+	q=a/b;
+	printf("%f + %f = %f\n",a,b,s);
+	printf("%f - %f = %f\n",a,b,d);
+	printf("%f * %f = %f\n",a,b,m);
+	printf("%f / %f = %f\n",a,b,q);
+	printf("\n*******\n\n\n\n");
+
+
+}
